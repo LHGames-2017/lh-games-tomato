aaeba924d507b001f0506ee087415ed8b53b9b71
Eric <sida.li@polymtl.ca>
2017-10-07 12:55:05 -0400

    [feature] player.isFullCapacity()

--- app/player.ts
+++ app/player.ts
@@ -1,20 +1,33 @@
 import { GameInfo, Tile, Point, IPlayer } from './interfaces';
 
 export class Player implements IPlayer {
-    Name: string;
-    AttackPower: number;
-    CarriedResources: number;
-    CarryingCapacity: number;
-    Defence: number;
-    Health: number;
-    MaxHealth: number;
-    Score: number;
-    TotalResources: number;
-    Position: Point;
-    HouseLocation: Point;
+    public Name: string;
+    public AttackPower: number;
+    public CarriedResources: number;
+    public CarryingCapacity: number;
+    public Defence: number;
+    public Health: number;
+    public MaxHealth: number;
+    public Score: number;
+    public TotalResources: number;
+    public Position: Point;
+    public HouseLocation: Point;
 
     constructor(map: Tile[][], gameInfo: GameInfo) {
         this.Name = gameInfo.Player.Name;
         this.AttackPower = gameInfo.Player.AttackPower;
+        this.CarriedResources = gameInfo.Player.CarriedResources;
+        this.CarryingCapacity = gameInfo.Player.CarryingCapacity;
+        this.Defence = gameInfo.Player.Defence;
+        this.Health = gameInfo.Player.Health;
+        this.MaxHealth = gameInfo.Player.MaxHealth;
+        this.Score = gameInfo.Player.Score;
+        this.TotalResources = gameInfo.Player.TotalResources;
+        this.Position = gameInfo.Player.Position;
+        this.HouseLocation = gameInfo.Player.HouseLocation;
+    }
+
+    public isFullCapacity(): boolean {
+        return this.CarriedResources + 100 >= this.CarryingCapacity;
     }
 }

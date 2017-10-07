f988d9c9823013605dbd63c66a578a6bc659c231
AudreyL <audrey.labrie@polymtl.ca>
2017-10-07 13:03:29 -0400

    get closest point method done

--- app/navigation.ts
+++ app/navigation.ts
@@ -1,4 +1,4 @@
-import { GameInfo, Tile, Point } from './interfaces';
+import { GameInfo, Tile, Point, TileContent } from './interfaces';
 
 export class Navigation {
     public static getRoute(map: Tile[][], gameInfo: GameInfo, target: Point): Point {
@@ -12,6 +12,27 @@ export class Navigation {
 
     public static getClosestRessource(map: Tile[][], gameInfo: GameInfo): Point {
         // find the closest ressource
-        return new Point(25, 27);
+        const resources: Array<Point> = [];
+        const currentPosition: Point = gameInfo.Player.Position;
+        let minDistPoint: Point;
+        let minDistance = 0;
+
+        for (let i = 0; i <  map.length; i++) {
+            for (let j = 0; j < map[i].length; j++) {
+                if (map[i][j].Content === TileContent.Resource) {
+                    if (minDistance === 0) {
+                        minDistPoint = map[i][j].Position;
+                        minDistance =  currentPosition.Distance(minDistPoint);
+                    } else {
+                        if(currentPosition.Distance(map[i][j].Position) < minDistance) {
+                            minDistance = currentPosition.Distance(resources[i]);
+                            minDistPoint = map[i][j].Position;
+                        }
+                    }
+                }
+            }
+        }
+
+        return minDistPoint;
     }
 }

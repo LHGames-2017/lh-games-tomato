b358f3764ea82192b1db291599345a0936935367
AudreyL <audrey.labrie@polymtl.ca>
2017-10-07 13:06:02 -0400

    added js for push to work...

--- out/navigation.js
+++ out/navigation.js
@@ -11,7 +11,27 @@ class Navigation {
     }
     static getClosestRessource(map, gameInfo) {
         // find the closest ressource
-        return new interfaces_1.Point(25, 27);
+        const resources = [];
+        const currentPosition = gameInfo.Player.Position;
+        let minDistPoint;
+        let minDistance = 0;
+        for (let i = 0; i < map.length; i++) {
+            for (let j = 0; j < map[i].length; j++) {
+                if (map[i][j].Content === interfaces_1.TileContent.Resource) {
+                    if (minDistance === 0) {
+                        minDistPoint = map[i][j].Position;
+                        minDistance = currentPosition.Distance(minDistPoint);
+                    }
+                    else {
+                        if (currentPosition.Distance(map[i][j].Position) < minDistance) {
+                            minDistance = currentPosition.Distance(resources[i]);
+                            minDistPoint = map[i][j].Position;
+                        }
+                    }
+                }
+            }
+        }
+        return minDistPoint;
     }
 }
 exports.Navigation = Navigation;
